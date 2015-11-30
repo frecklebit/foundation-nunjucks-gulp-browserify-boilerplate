f434e3aef2057e4594693fc8ac512fa19ed0b195
Adam Jenkins <adamcole83@gmail.com>
2015-11-30 13:56:16 -0600

    Added lint task for script development

---
+++ gulp/tasks/lint.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import config from '../config';
+import gulp   from 'gulp';
+import jshint from 'gulp-jshint';
+
+gulp.task('lint', function() {
+  
+  return gulp.src( config.scripts.src )
+    .pipe( jshint() )
+    .pipe( jshint.reporter( 'jshint-stylish' ) );
+    
+});
