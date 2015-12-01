d3d513fe6a84f4ba9646890a85eaadb511291fe2
Adam Jenkins <adamcole83@gmail.com>
2015-12-01 12:43:53 -0600

    Added a data file for Nunjucks

---
+++ src/data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export default {
+  
+  title: "My New Project",
+  description: "This is a template for your new project, use it wisely",
+  keywords: "this, that, other shit",
+  
+};
